d857dc4d0d27475fad67269b62889e125733a38e
Deepak-Verma-556 <deepakkhudia46015@gmail.com>
2024-10-15 12:16:23 +0530

    add images in folder and add custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'text-2xsm': '10px',
+        'text-xsm': '12px',
+        'text-5xl': '56px',
+      },
+      colors: {
+        darkgray:'#333333',
+        lightgray:'#8f8f8f',
+        orange:'#fa8443',
+        blue:'#347f90',
+      },
+      lineHeight: {
+        'leading-xl':'50px',
+        'leading-2xl':'69px',
+      }
+    },
   },
   plugins: [],
 }

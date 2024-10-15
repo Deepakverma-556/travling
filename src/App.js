71505ffb555afa33085b5615a8e54a40700bff67
Deepak-Verma-556 <deepakkhudia46015@gmail.com>
2024-10-15 14:24:06 +0530

    section 3 create

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Section2 from './components/Section2';
+import Section3 from './components/Section3';
 
 function App() {
   return (
     <>
       <Header />
       <Section2 />
+      <Section3/>
     </>
   );
 }
